23fa1182e18d9d42b8fb2d2c893c11a4e0dfebd7
Kevin Grimaldi <anthgrim@icloud.com>
2024-05-04 22:03:58 -0400

    custom readable stream

---
+++ streams/custom-readable/custom-readable.js
@@ -0,0 +1,45 @@
+const { Readable } = require('node:stream');
+const fs = require('node:fs');
+
+class FileReadStream extends Readable {
+  constructor(fileName) {
+    super();
+
+    this.fileName = fileName;
+    this.fd = null;
+  }
+
+  _construct(callback) {
+    fs.open(this.filename, (err, fd) => {
+      if (err) {
+        callback(err);
+      } else {
+        this.fd = fd;
+        callback();
+      }
+    });
+  }
+
+  _read(size) {
+    const buf = Buffer.alloc(size);
+    fs.read(this.fd, buf, 0, n, null, (err, bytesRead) => {
+      if (err) {
+        this.destroy(err);
+      } else {
+        this.push(bytesRead > 0 ? buf.slice(0, bytesRead) : null);
+      }
+    });
+  }
+
+  _destroy(err, callback) {
+    if (this.fd) {
+      fs.close(this.fd, (er) => callback(er || err));
+    } else {
+      callback(err);
+    }
+  }
+}
+
+const stream = new FileReadStream('');
+
+stream.read(13);
