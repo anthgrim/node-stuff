955249873cdf63e59afb0a4a31ce20cc2f8695c9
Kevin Grimaldi <anthgrim@icloud.com>
2024-05-04 21:05:20 -0400

    using pipeline

--- streams/copy/copy.js
+++ streams/copy/copy.js
@@ -1,26 +1,30 @@
 const fs = require('node:fs/promises');
+const { pipeline } = require('node:stream');
+const zlib = require('node:zlib');
 
 const timeLabel = 'copy';
 const origin = './origin.txt';
 const destination = './copy.txt';
 
 (async () => {
+  console.time(timeLabel);
   try {
-    console.time(timeLabel);
-
     const originFile = await fs.open(origin, 'r');
     const destFile = await fs.open(destination, 'w');
 
     const readStream = originFile.createReadStream();
     const writeStream = destFile.createWriteStream();
 
-    readStream.pipe(writeStream);
-
-    readStream.on('end', () => {
-      console.timeEnd(timeLabel);
+    pipeline(readStream, writeStream, (error) => {
+      if (error) {
+        console.log(error);
+        console.timeEnd(timeLabel);
+      } else {
+        console.log('Success');
+        console.timeEnd(timeLabel);
+      }
     });
   } catch (error) {
     console.log(error);
-    console.timeEnd(timeLabel);
   }
 })();
